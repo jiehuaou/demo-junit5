073946c8726796b6968b05e22ba8e2afc68345fb
albert ou <albert_ou@epam.com>
2021-12-23 14:34:19 +0800

    demo-junit5 : enum

--- src/test/java/com/example/demo/junit5test/EnumTests.java
+++ src/test/java/com/example/demo/junit5test/EnumTests.java
@@ -12,39 +12,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class EnumTests {
-    @Test
-    public void testFromString1(){
-        EnumSimpleType enumType = EnumSimpleType.valueOf("EPAM");
-        System.out.println(enumType);
-        Assertions.assertEquals(enumType, EnumSimpleType.EPAM);
-    }
 
-    @Test
-    public void testFromStringEx(){
-        Exception ex = Assertions.assertThrows(IllegalArgumentException.class, ()->{
-            EnumSimpleType enumType = EnumSimpleType.valueOf("EPAM-X");
-            System.out.println(enumType);
-        });
-        System.out.println(ex.getMessage());
-        Assertions.assertTrue(ex.getMessage().contains("EnumType.EPAM-X"));
-        Assertions.assertTrue(ex.getMessage().contains("No enum constant"));
-    }
+    @Getter
+    @Builder(toBuilder = true)
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @ToString
+    public static class SimpleDataDTO {
+        private String data;
+        private EnumSimpleType simple;  // use valueOf()
 
-    @Test
-    public void testDataWithEnum2String() throws JsonProcessingException {
-        DataWithEnum dataWithEnum = DataWithEnum.builder().data("foo").enumType(EnumSimpleType.EPAM).build();
-        ObjectMapper objectMapper = new ObjectMapper();
-        String ss = objectMapper.writeValueAsString(dataWithEnum);
-        System.out.println(ss);
-    }
-    //{"data":"foo","enumType":"EPAM"}
-    @Test
-    public void testDataWithEnumFromString() throws JsonProcessingException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        DataWithEnum dataWithEnum = objectMapper.readValue("{\"data\":\"foo\",\"enumType\":\"EPAM\"}", DataWithEnum.class);
-        System.out.println(dataWithEnum);
-        Assertions.assertEquals(dataWithEnum.getEnumType(), EnumSimpleType.EPAM);
-        Assertions.assertEquals(dataWithEnum.getData(), "foo");
     }
 
     public static class DataDTO{
@@ -61,20 +38,52 @@ public class EnumTests {
     }
 
     @Test
+    public void testFromString1(){
+        EnumSimpleType enumType = EnumSimpleType.valueOf("EPAM");
+        System.out.println(enumType);
+        Assertions.assertEquals(enumType, EnumSimpleType.EPAM);
+    }
+
+    @Test
+    public void testFromStringException(){
+        Exception ex = Assertions.assertThrows(IllegalArgumentException.class, ()->{
+            EnumSimpleType enumType = EnumSimpleType.valueOf("EPAM-X");
+            System.out.println(enumType);
+        });
+        System.out.println(ex.getMessage());
+        Assertions.assertTrue(ex.getMessage().contains("EPAM-X"));
+        Assertions.assertTrue(ex.getMessage().contains("No enum constant"));
+    }
+
+    @Test
+    public void testDataWithEnum2String() throws JsonProcessingException {
+        SimpleDataDTO dataWithEnum = SimpleDataDTO.builder().data("foo").simple(EnumSimpleType.EPAM).build();
+        ObjectMapper objectMapper = new ObjectMapper();
+        String ss = objectMapper.writeValueAsString(dataWithEnum);
+        System.out.println(ss);
+    }
+    //{"data":"foo","enumType":"EPAM"}
+    @Test
+    public void testDataWithEnumFromString() throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        SimpleDataDTO dataWithEnum = objectMapper.readValue("{\"data\":\"foo\",\"simple\":\"EPAM\"}",
+                SimpleDataDTO.class);
+        System.out.println(dataWithEnum);
+        Assertions.assertEquals(dataWithEnum.getSimple(), EnumSimpleType.EPAM);
+        Assertions.assertEquals(dataWithEnum.getData(), "foo");
+    }
+
+
+
+    @Test
     public void testComplexData() throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
-        DataDTO dataWithEnum = objectMapper.readValue("{\"data\":\"foo\",\"complex\":2}", DataDTO.class);
+        DataDTO dataWithEnum = objectMapper.readValue("{\"data\":\"foo\",\"complex\":2}",
+                DataDTO.class);
         System.out.println(dataWithEnum);
+        Assertions.assertEquals(dataWithEnum.complex, EnumComplexType.WORLD);
+        Assertions.assertEquals(dataWithEnum.data, "foo");
     }
 
-    @Getter
-    @Builder(toBuilder = true)
-    @NoArgsConstructor
-    @AllArgsConstructor
-    @ToString
-    public static class DataWithEnum {
-        private String data;
-        private EnumSimpleType enumType;  // use valueOf()
 
-    }
 }
